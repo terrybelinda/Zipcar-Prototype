85004cf57b3ed835a8f9c37c0e958b0999c0e217
Ashwini Talele <ashtalele70@gmail.com>
2020-03-01 15:32:19 -0800

    Backend starter commit

---
+++ backend 3.31.05 PM/src/main/java/com/rent/controller/CustomerController.java
@@ -0,0 +1,25 @@
+package com.rent.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rent.model.Customer;
+import com.rent.service.CustomerService;
+
+@RestController
+@RequestMapping("/api")
+public class CustomerController {
+	
+	@Autowired
+	private CustomerService customerService;
+	
+	@GetMapping("/customer")
+	public List<Customer> get() {
+		return customerService.get();
+	}
+}
+ 
