d3585c25c9d64b7d44eeacd8a08ab9a50199a443
terrylinda13@gmail.com <terrylinda13@gmail.com>
2020-03-28 19:49:58 -0700

    Changes for sign up and login

---
+++ frontend/src/components/rentalLogin/rentalLogin.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+class RentalLogin extends Component {
+  render() {
+    return (
+      <Container className="m-5 d-flex justify-content-center">
+        <Form>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+          </Form.Group>
+
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <Form.Group controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Check me out" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </Container>
+    );
+  }
+}
+export default RentalLogin;
