7650b53a887323a8bdc31745c2f0307064817502
terrylinda13@gmail.com <terrylinda13@gmail.com>
2020-03-29 08:34:39 -0700

    nav bar added

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -6,11 +6,13 @@ import cookie from "react-cookies";
 import { Redirect } from "react-router";
 import RentalLogin from "./rentalLogin/rentalLogin";
 import SignUp from "./signUp/signUp";
+import Navigation from "./Navbar/Navbar";
 
 class Home extends Component {
   render() {
     return (
       <div>
+        <Route path="/" component={Navigation} />
         <Route exact path="/login" component={RentalLogin} />
         <Route exact path="/signup" component={SignUp} />
       </div>
