0acd6a0da5730ab3cc0859032f006b4d1de14060
Ashwini Talele <ashtalele70@gmail.com>
2020-04-01 23:43:52 -0700

    Integrate redux into react

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,8 @@ import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 import "bootstrap/dist/css/bootstrap.min.css";
+import store from "./store";
+import { Provider } from "react-redux";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById("root"));
 registerServiceWorker();
