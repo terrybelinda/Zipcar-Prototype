5eaac44e225a3e889a8864dd4700beb9c102f5d3
terrylinda13@gmail.com <terrylinda13@gmail.com>
2020-04-04 14:27:23 -0700

    Changes for feedback component

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -6,6 +6,7 @@ import cookie from "react-cookies";
 import { Redirect } from "react-router";
 import RentalLogin from "./rentalLogin/rentalLogin";
 import SignUp from "./signUp/signUp";
+import Feedback from "./feedback/feedback";
 import Navigation from "./Navbar/Navbar";
 
 class Home extends Component {
@@ -15,6 +16,7 @@ class Home extends Component {
         <Route path="/" component={Navigation} />
         <Route exact path="/login" component={RentalLogin} />
         <Route exact path="/signup" component={SignUp} />
+        <Route exact path="/feedback" component={Feedback} />
       </div>
     );
   }
