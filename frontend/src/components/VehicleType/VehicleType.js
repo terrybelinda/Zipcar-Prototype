a6b68b8715eee51844727ea7865bc3697245962e
terrylinda13@gmail.com <terrylinda13@gmail.com>
2020-04-17 14:16:53 -0700

    alignment changes for cards in vehicle type

--- frontend/src/components/VehicleType/VehicleType.js
+++ frontend/src/components/VehicleType/VehicleType.js
@@ -32,13 +32,13 @@ class VehicleType extends Component {
     axios
       .get("http://localhost:8080/api/allvehicletype")
       .then((res) => {
-        //if (res.status === 200) {
-        console.log(res.data);
-        if (res.data) {
+        if (res.status === 200) {
           console.log(res.data);
-          this.setState({ type: res.data });
+          if (res.data) {
+            console.log(res.data);
+            this.setState({ type: res.data });
+          }
         }
-        //}
       })
       .catch((err) => {});
   };
@@ -89,81 +89,56 @@ export const Types = (props) => {
 */
   render() {
     const list = this.state.type.map((item, index) => (
-      <Card
-        bg="light"
-        style={{ width: "18rem" }}
-        className="mt-2 border border-primary"
-        key={item.id}
-      >
-        <Card.Header as="h5">
-          licence #: {item.license_no}
-          Make: {item.make}
-          Model: {item.model}
-        </Card.Header>
+      <Col sm="3">
+        <Card
+          bg="light"
+          //style={{ width: "18rem" }}
+          className="mt-2 border border-primary"
+          key={item.id}
+        >
+          <Card.Header as="h5">Type: {item.vehicle_type}</Card.Header>
 
-        <Card.Body>
-          <Row>
-            <Card.Text id="year"> Year :{item.year}</Card.Text>
-            Status:
-            {item.status == 0 ? (
-              <Button disabled size="sm" variant="success">
-                Active
-              </Button>
-            ) : (
-              <Button disabled size="sm" variant="danger">
-                Inactive
-              </Button>
-            )}
-          </Row>
-          <Row>
-            <Card.Text id="regisration_expiry">
-              <b>Registration Expiry:</b> {item.regisration_expiry}
-            </Card.Text>
-          </Row>
+          <Card.Body>
+            <Row>
+              Status:
+              {item.status == 0 ? (
+                <Button disabled size="sm" variant="success">
+                  Active
+                </Button>
+              ) : (
+                <Button disabled size="sm" variant="danger">
+                  Inactive
+                </Button>
+              )}
+            </Row>
 
-          <Row>
-            <Card.Text id="vid">
-              <b>Vehicle ID: </b> {item.vid}
-            </Card.Text>
-          </Row>
+            <Row>
+              <Card.Text id="vid">
+                <b>price : </b> {item.price}
+              </Card.Text>
+            </Row>
 
-          <Row>
-            <Card.Text id="current_mileage">
-              <b>Miles:</b> {item.current_mileage}
-            </Card.Text>
-            <Card.Text id="condition">Condition: {item.condition}</Card.Text>
-          </Row>
-
-          <Row>
-            <Card.Text id="vehicle_type">
-              <b>vehicle type:</b>
-              {item.vehicle_type}
-            </Card.Text>
-            <Card.Text id="rental_location">
-              <b>Rental Location:</b>
-              {item.rental_location}
-            </Card.Text>
-          </Row>
-
-          <Card.Link href="#" onClick={() => this.showModal(index)}>
-            Edit
-          </Card.Link>
-          <Card.Link href="#" onClick={() => this.removeItem(item)}>
-            Delete
-          </Card.Link>
-        </Card.Body>
-      </Card>
+            <Row>
+              <Card.Text id="condition">hours: {item.hours}</Card.Text>
+            </Row>
+            <Card.Link href="#" onClick={() => this.showModal(index)}>
+              Edit
+            </Card.Link>
+            <Card.Link href="#" onClick={() => this.removeItem(item)}>
+              Delete
+            </Card.Link>
+          </Card.Body>
+        </Card>
+      </Col>
     ));
 
     const requiredItem = this.state.requiredItem;
     let modalData = this.state.type[requiredItem];
     return (
-      <div style={{ paddingLeft: "300px" }}>
+      <div>
         <Container>
-          <Button variant="outline-primary">Add</Button>
-          <Row>
-            <Col md={4}>{list}</Col>
-          </Row>
+          <Button variant="primary">Add Vehicle Type</Button>
+          <Row>{list}</Row>
         </Container>
         <Modal show={this.state.show} onHide={this.hideModal} animation={false}>
           <Modal.Header closeButton>
