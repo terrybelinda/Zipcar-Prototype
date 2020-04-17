73e160c7eef59ab0fad336641ae6628390fb8bea
terrylinda13@gmail.com <terrylinda13@gmail.com>
2020-04-16 22:15:49 -0700

    changes to display vehicles

--- frontend/src/components/VehicleType/VehicleType.js
+++ frontend/src/components/VehicleType/VehicleType.js
@@ -1,21 +1,47 @@
 import React from "react";
-import { Card, Button, Modal, Form, Alert, Badge } from "react-bootstrap";
+import {
+  Container,
+  Card,
+  Row,
+  Col,
+  Button,
+  Modal,
+  Form,
+} from "react-bootstrap";
 import { useRef, useState, Component } from "react";
+import axios from "axios";
 
 class VehicleType extends Component {
   constructor(props) {
     super(props);
     this.state = {
       requiredItem: 0,
-      type: [
-        { id: 1, vehicle_type: "truck" },
-        { id: 2, vehicle_type: "car" },
-        { id: 3, vehicle_type: "bike" },
-      ],
+      type: [],
       show: false,
     };
     this.saveModalDetails = this.saveModalDetails.bind(this);
   }
+  componentDidMount() {
+    this.getVehicles();
+  }
+
+  getVehicles = () => {
+    axios.defaults.headers.common["x-auth-token"] = localStorage.getItem(
+      "token"
+    );
+    axios
+      .get("http://localhost:8080/api/allvehicletype")
+      .then((res) => {
+        //if (res.status === 200) {
+        console.log(res.data);
+        if (res.data) {
+          console.log(res.data);
+          this.setState({ type: res.data });
+        }
+        //}
+      })
+      .catch((err) => {});
+  };
   removeItem(item) {
     const newItems = this.state.type.filter((type) => {
       return type !== item;
@@ -44,7 +70,7 @@ class VehicleType extends Component {
   // }
 
   saveModalDetails(e) {
-    console.log(e.target.elements[0].value);
+    //console.log(e.target.elements[0].value);
     // make api call
     // setstate
     const requiredItem = this.state.requiredItem;
@@ -65,12 +91,60 @@ export const Types = (props) => {
     const list = this.state.type.map((item, index) => (
       <Card
         bg="light"
-        style={{ width: "45rem", paddingRight: "100px" }}
-        className="mt-2"
+        style={{ width: "18rem" }}
+        className="mt-2 border border-primary"
         key={item.id}
       >
+        <Card.Header as="h5">
+          licence #: {item.license_no}
+          Make: {item.make}
+          Model: {item.model}
+        </Card.Header>
+
         <Card.Body>
-          <Card.Text id="type">{item.vehicle_type}</Card.Text>
+          <Row>
+            <Card.Text id="year"> Year :{item.year}</Card.Text>
+            Status:
+            {item.status == 0 ? (
+              <Button disabled size="sm" variant="success">
+                Active
+              </Button>
+            ) : (
+              <Button disabled size="sm" variant="danger">
+                Inactive
+              </Button>
+            )}
+          </Row>
+          <Row>
+            <Card.Text id="regisration_expiry">
+              <b>Registration Expiry:</b> {item.regisration_expiry}
+            </Card.Text>
+          </Row>
+
+          <Row>
+            <Card.Text id="vid">
+              <b>Vehicle ID: </b> {item.vid}
+            </Card.Text>
+          </Row>
+
+          <Row>
+            <Card.Text id="current_mileage">
+              <b>Miles:</b> {item.current_mileage}
+            </Card.Text>
+            <Card.Text id="condition">Condition: {item.condition}</Card.Text>
+          </Row>
+
+          <Row>
+            <Card.Text id="vehicle_type">
+              <b>vehicle type:</b>
+              {item.vehicle_type}
+            </Card.Text>
+            <Card.Text id="rental_location">
+              <b>Rental Location:</b>
+              {item.rental_location}
+            </Card.Text>
+          </Row>
+
           <Card.Link href="#" onClick={() => this.showModal(index)}>
             Edit
           </Card.Link>
@@ -83,11 +157,14 @@ export const Types = (props) => {
 
     const requiredItem = this.state.requiredItem;
     let modalData = this.state.type[requiredItem];
-    console.log("hello");
     return (
       <div style={{ paddingLeft: "300px" }}>
-        <Button variant="outline-primary">Add</Button>
-        {list}
+        <Container>
+          <Button variant="outline-primary">Add</Button>
+          <Row>
+            <Col md={4}>{list}</Col>
+          </Row>
+        </Container>
         <Modal show={this.state.show} onHide={this.hideModal} animation={false}>
           <Modal.Header closeButton>
             <Modal.Title>Add Vehicles</Modal.Title>
@@ -98,7 +175,6 @@ export const Types = (props) => {
                 <Form.Label>vehicle</Form.Label>
                 <Form.Control
                   type="vehicle"
-                  //onChange={this.saveVehicleType}
                   placeholder={modalData && modalData.vehicle_type}
                 />
               </Form.Group>
